65f5a514e52d146a90d3c1c73a352df60f82f475
Wang Yaoshen <wangyaoshen@bgi.com>
2021-12-30 11:15:07 +0800

    :new::bento:add Version()

--- version.go
+++ version.go
@@ -1,6 +1,7 @@
 package version
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
@@ -17,5 +18,14 @@ func LogVersion() {
 	log.Printf("Build Time     : %s\n", buildStamp)
 	log.Printf("Golang Version : %s\n", golangVersion)
 	var hostName, err = os.Hostname()
-	log.Printf("Hostname       : %s%v\n", hostName, err)
+	log.Printf("Current Host   : %s%v\n", hostName, err)
+}
+
+// Version print out version info
+func Version() {
+	fmt.Printf("Git Describe   : %s\n", gitDescribe)
+	fmt.Printf("Build Time     : %s\n", buildStamp)
+	fmt.Printf("Golang Version : %s\n", golangVersion)
+	var hostName, err = os.Hostname()
+	fmt.Printf("Current Host   : %s%v\n", hostName, err)
 }
