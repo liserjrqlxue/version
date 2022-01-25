7b5e6b5016889f5d811c877252f7ea4927e233b4
Wang Yaoshen <wangyaoshen@bgi.com>
2022-01-25 15:04:13 +0800

    :wrench::children_crossing:go build default no '-x'

--- vb/main.go
+++ vb/main.go
@@ -22,14 +22,22 @@ var tagsArgs = [][]string{
 	{"rev-parse", "HEAD"},
 }
 
-var ldflags = flag.String(
-	"ldflags",
-	"",
-	"go build ldflags",
+var (
+	x = flag.Bool(
+		"x",
+		false,
+		"print the commands.",
+	)
+	ldflags = flag.String(
+		"ldflags",
+		"",
+		"go build ldflags",
+	)
 )
 
 func main() {
 	flag.Parse()
+
 	var gitBranch string
 	for _, args := range currentBranchAargs {
 		var currentBranch, err = exec.Command("git", args...).Output()
@@ -49,7 +57,15 @@ func main() {
 	var gitDescribe = gitBranch + ":" + gitTag
 	var date = time.Now().Format("2006-01-02_15:04:05PM")
 	var goVersion = strings.Trim(string(HandleError(exec.Command("go", "version").Output()).([]byte)), "\n")
-	var build = exec.Command("go", "build", "-x", "-ldflags", fmt.Sprintf("-X 'github.com/liserjrqlxue/version.gitDescribe=%s' -X 'github.com/liserjrqlxue/version.buildStamp=%s' -X 'github.com/liserjrqlxue/version.golangVersion=%s' %s", gitDescribe, date, goVersion, *ldflags))
+	var ldFlags = fmt.Sprintf("-X 'github.com/liserjrqlxue/version.gitDescribe=%s' -X 'github.com/liserjrqlxue/version.buildStamp=%s' -X 'github.com/liserjrqlxue/version.golangVersion=%s' %s", gitDescribe, date, goVersion, *ldflags)
+
+	var args = []string{"build", "-ldflags", ldFlags}
+
+	if *x {
+		args = append(args, "-x")
+	}
+
+	var build = exec.Command("go", args...)
 	fmt.Println(build.String())
 	build.Stdout = os.Stdout
 	build.Stderr = os.Stderr
